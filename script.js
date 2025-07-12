51d503cb822386ca30b6add4dde32bf00b82b00b
liljeam <jamesomtoyinbo@gmail.com>
2025-07-12 15:05:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle mobile menu
+function toggleMobileMenu() {
+  const menu = document.getElementById("mobile-menu");
+  const icon = document.getElementById("hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
