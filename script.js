3aa613bf67782d49fe7e75609b60542a18dd14bf
liljeam <jamesomtoyinbo@gmail.com>
2025-07-12 16:23:30 -0500

    fixes

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+const menu = document.getElementById("mobile-menu");
+const icon = document.getElementById("hamburger-icon");
+
+
+
+
+
+
+
+
 // Toggle mobile menu
 function toggleMobileMenu() {
-  const menu = document.getElementById("mobile-menu");
-  const icon = document.getElementById("hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
 
+// Toggle mobile menu for button click
+function toggleMobileMenubtn() {
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
